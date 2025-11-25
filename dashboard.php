8d8107e9c34567e0298294b7aeb0503a40ac561e
Lojonnz <zoneyt77@gmail.com>
2025-11-25 17:23:31 +0800

    memodifikasi dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
+if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
+    header("Location: index.php");
+    exit;
+}
+?>
+
+<?php
+session_start();
 if (!isset($_SESSION['username'])) {
     $_SESSION['username'] = "Lorapz Store";
 }
